1b6b3e08fbd7ea2965cadaf7f46b4e6c1006e3aa
Stephan Haditsch <stephan-haditsch@hotmail.com>
2020-10-13 15:55:31 +0200

    simplified basic 1

--- Basic 1/basic.js
+++ Basic 1/basic.js
@@ -1,9 +1,8 @@
-var numArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var numArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var _i = 0, numArray1_1 = numArray1; _i < numArray1_1.length; _i++) {
-    var number = numArray1_1[_i];
-    for (var _a = 0, numArray2_1 = numArray2; _a < numArray2_1.length; _a++) {
-        var number2 = numArray2_1[_a];
-        document.write(number + "*" + number2 + " = ", number * number2 + "<br>");
+var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var _i = 0, numArray_1 = numArray; _i < numArray_1.length; _i++) {
+    var number = numArray_1[_i];
+    for (var _a = 0, numArray_2 = numArray; _a < numArray_2.length; _a++) {
+        var number2 = numArray_2[_a];
+        document.write(number + "X" + number2 + " = " + number * number2 + "<br>");
     }
 }
